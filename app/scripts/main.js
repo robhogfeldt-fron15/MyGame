4eb8fa0226e1ca1042fe02762c6c784366e54bda
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2016-01-01 11:29:09 +0100

    test for two players

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,14 @@ var dbRef = new Firebase('https://fron15game.firebaseio.com/');
 var emailField = $('#email');
 var pwdField = $('#pwd');
 
+
+//Some Globals
+var firstWord = [];
+var secondWord = [];
+var thirdWord = [];
+var word=[];
+var array = [];
+
 //REGISTER NEW USER
 $("#regUser").on("click", function(){
 dbRef.createUser({
@@ -46,7 +54,43 @@ password    : $("#pwd").val(),
 })
 });
 
+function listLoggedInUsers() {
+  var list = $('<ul/>').appendTo('#showusers');
+dbRef.child("users").once('value', function(dataSnapshot) {
 
+var users = dataSnapshot.val();
+
+if (users) {
+  jQuery.each(users, function(i, val) {
+  var me =  dbRef.getAuth().password.email;
+    if (val.email !== me) {
+      list.append('<li class="playUser">' + val.email +'</li>');
+      $( ".playUser" ).on( "click", function() {
+      createGame( me, val.email );
+  });
+    }
+
+
+    });
+}
+
+
+
+  console.log(users);
+});
+}
+
+function createGame(playerOne, playerTwo ) {
+  // body...
+  var newGameRef = dbRef.child("game");
+  newGameRef.set({
+
+      playerOne : playerOne,
+      playerTwo : playerTwo
+
+  });
+
+}
 
 
 
@@ -57,19 +101,41 @@ var data={"sentences":[
             "third":"bil"
         },
         {
-            "first":"John",
-            "second":"Jones",
-            "third":"2010"
+            "first":"kvinna",
+            "second":"skjuter",
+            "third":"älg"
         }
 ]}
 
 
+
+
+
+
 $('#startBtn').on('click', function(){
 
+array = data.sentences[Math.floor(Math.random()*data.sentences.length)];
+console.log(array);
+var gamewordRef = dbRef.child("gameword");
+gamewordRef.set({
+  array
+});
 
-  var array = data.sentences[0];
-  var keys = Object.keys(array);
-
+var playerwordRef = dbRef.child("playerword");
+playerwordRef.set({
+  first:{
+    word : [],
+    correctWord : array.first
+  },
+  second:{
+    word : [],
+    correctWord : array.second
+  },
+  third:{
+    word : [],
+    correctWord : array.third
+  }
+});
 
 $.each(array, function( i, l ){
     var line = "#" + i;
@@ -79,6 +145,9 @@ $.each(array, function( i, l ){
 
   });
 
+if (dbRef.getAuth().password.email = "test@hot.se") {
+$(".playerOne").css("display: none");
+}
 
 });
 
@@ -100,43 +169,36 @@ function createPlayBricks(word) {
 
       }
 
-      var firstWord = [];
-      var secondWord = [];
-      var thirdWord = [];
-      var id = 0
-      var word=[];
+
 
 function createBricks(line, length) {
 
     var line = $(line);
     var play = $('#chars');
       for (var i = 0; i < length; i++) {
-
         var brick = $('<div/>')
-
             .addClass('square droppable squaredotted')
             .appendTo(line);
-
-      }
+          }
       $(".squaredotted").droppable({
 
           drop: function(event, ui) {
            var dropIndex = $(this).index();
            var parentId = $(this).parent().attr('id');
-           console.log(parentId);
            var char =  $(ui.draggable).html();
-            word[dropIndex] = char;
-          var w =  switchResult(parentId, char, dropIndex)
-          if (w.word === w.correctWord) {
-          console.log('success');
-            }
-            console.log(w);
-            },
+               word[dropIndex] = char;
+
+           var myWord = switchResult(parentId, char, dropIndex)
+               updateFirebaseAdd(myWord, parentId)
+        },
            out: function(event, ui) {
              var drop = $(this).index();
-           var char =  $(ui.draggable).html();
-            word[drop] = 0;
-              console.log(word);
+             var char =  $(ui.draggable).html();
+             var parentId = $(this).parent().attr('id');
+             var myWord =  switchResult(parentId, char, drop);
+                 myWord.word[drop] = 0;
+                 updateFirebaseAdd(myWord, parentId);
+
           }
 
         });
@@ -144,37 +206,56 @@ function createBricks(line, length) {
 }
 
 
+var playerwordRef = new Firebase('https://fron15game.firebaseio.com/playerword/');
 
-// function isRight(line, char, id) {
-//
-//   var array = data.sentences[0];
-//   var choosenLine = switchResult(line.id, array, char);
-//   console.log(choosenLine);
-//
-//
-//
-//   if (choosenLine.word === choosenLine.correctWord) {
-// console.log('success');
-//   }
-//
-// }
+function updateFirebaseAdd(myWord, parentId) {
+
+var playerwordRef = new Firebase('https://fron15game.firebaseio.com/playerword/' + parentId);
+switch(parentId) {
+  case "first":
+      playerwordRef.update({ first: myWord.word})
+      break;
+  case "second":
+      playerwordRef.update({ second: myWord.word})
+      break;
+  case "third":
+      playerwordRef.update({ third: myWord.word})
+      break;
+  default:
+      console.log("none");
+}
+
+// TODO: Cchange CSS if word is correct
+playerwordRef.on("child_changed", function(snapshot) {
+  var newWord = snapshot.val();
+
+  if (newWord.join("") === myWord.correctWord) {
+  console.log("newWord)");
+  }
+
+});
+}
+
+
+
 
 function switchResult(line, char, dropIndex){
-  console.log(line + char + dropIndex);
-    var array = data.sentences[0];
+
+
   switch(line) {
     case "first":
         firstWord[dropIndex] = char;
         var obj = {
-               word:  firstWord.join(""),
+               word:  firstWord,
                correctWord: array.first
            };
+
            return obj;
         break;
     case "second":
         secondWord[dropIndex] = char;
           var obj = {
-                 word:  secondWord.join(""),
+                 word:  secondWord,
                  correctWord: array.second
              };
             return obj;
@@ -182,7 +263,7 @@ function switchResult(line, char, dropIndex){
     case "third":
        thirdWord[dropIndex] = char;
          var obj = {
-                word:  thirdWord.join(""),
+                word:  thirdWord,
                 correctWord: array.third
             };
            return obj;
@@ -190,9 +271,4 @@ function switchResult(line, char, dropIndex){
     default:
         console.log("none");
 }
-}
-
-
-function createWordfromArr(){
-
 }
